c20a84eb9129a41c5d83e8431844bb947ec1ef20
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-11-15 01:14:11 +0100

    Added price property.

--- TestClassLibraryQ/Medicine.cs
+++ TestClassLibraryQ/Medicine.cs
@@ -11,5 +11,6 @@ namespace TestClassLibraryQ
         public int Id { get; set; }
         public string Name { get; set; }
         public string Supplier { get; set; }
+        public double Price { get; set; }
     }
 }
