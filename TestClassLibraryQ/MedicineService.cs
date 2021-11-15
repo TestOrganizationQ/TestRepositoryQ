0b873692805de3b1f3bf42579f07ae252fe07bfb
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2021-11-15 02:49:16 +0100

    SmellyMedicine method

---
+++ TestClassLibraryQ/MedicineService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibraryQ
+{
+    public class MedicineService
+    {
+        public int SmellyMedicine()
+        {
+            int a;
+            int b = 8;
+            int c;
+            a = 5 + b + 2;
+            a = 8;
+            return 6;
+        }
+    }
+}
