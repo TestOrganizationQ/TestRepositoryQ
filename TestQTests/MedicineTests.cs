1a93f3f76e839117d0db8ec39d0eb43e4207364c
srdjansukovic <sukovic.ra3.2018@uns.ac.rs>
2021-11-17 21:58:55 +0100

    tests updated

--- TestQTests/MedicineTests.cs
+++ TestQTests/MedicineTests.cs
@@ -13,7 +13,6 @@ namespace TestQTests
             int number = medicineService.GetEvenMedicine(1004);
 
             Assert.True(number % 2 == 0);
-
         }
 
         [Fact]
@@ -23,27 +22,24 @@ namespace TestQTests
             int number = medicineService.GetEvenMedicine(5);
 
             Assert.False(number % 2 == 0);
-
         }
 
         [Fact]
         public void Odd_number_passing_test()
         {
             MedicineService medicineService = new MedicineService();
-            int number = medicineService.GetEvenMedicine(1003);
+            int number = medicineService.GetOddMedicine(1003);
 
             Assert.True(number % 2 != 0);
-
         }
 
         [Fact]
         public void Odd_number_failing_test()
         {
             MedicineService medicineService = new MedicineService();
-            int number = medicineService.GetEvenMedicine(6);
+            int number = medicineService.GetOddMedicine(6);
 
             Assert.False(number % 2 != 0);
-
         }
     }
 }
